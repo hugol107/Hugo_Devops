9dadc70b0203cfceaaedf741ef194d586065a1c1
hugol107 <167015853+hugol107@users.noreply.github.com>
2024-07-10 16:33:12 +0100

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     version = os.getenv('APP_VERSION', '1.0')
-    return f'Hello, Marc! This is a test version. Version: {version}'
+    return f'Hello, Hugo here! This is a test version. Version: {version}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
