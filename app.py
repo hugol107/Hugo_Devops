e29c15afdc3b4784845bfa94f52c8d9932739b59
hugol107 <167015853+hugol107@users.noreply.github.com>
2024-07-11 17:55:35 +0100

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     version = os.getenv('APP_VERSION', '1.0')
-    return f'Hello, World! Version: {version}'
+    return f'Hello, Hugo! Version: {version}'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
