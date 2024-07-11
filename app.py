0b6b5857f0d10d233dbcf35cb677ec5e79ccf5a6
hugol107 <167015853+hugol107@users.noreply.github.com>
2024-07-11 11:56:48 +0100

    app.py

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ def home():
     return f'Hello, Hugo here! This is a test version. Version: {version}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
