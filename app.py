4ba7500d746315774e39318c2d5d196d34c6bf42
hugol107 <167015853+hugol107@users.noreply.github.com>
2024-07-11 12:14:07 +0100

    app.py

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     version = os.getenv('APP_VERSION', '1.0')
-    return f'Hello, Hugo here! This is a test version. Version: {version}'
+    return f'Hello, World! Version: {version}'
 
 if __name__ == '__main__':
+    # Get the port from the environment variable 'PORT' or use 5000 as default
     port = int(os.environ.get('PORT', 5000))
+    # Run the app on all available interfaces (0.0.0.0)
     app.run(host='0.0.0.0', port=port)
